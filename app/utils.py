ff73367278253f45bcf731bb6f2841bdf72854aa
amancrackpot <59706418+amancrackpot@users.noreply.github.com>
2020-02-12 01:36:46 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,28 +1,142 @@
-from fastai import *
-from fastai.vision import *
+import torch
+from fastai.callbacks import *
 
-class FeatureLoss_Wass():
+class TransformerNet(torch.nn.Module):
     def __init__(self):
+        super(TransformerNet, self).__init__()
+        # Initial convolution layers
+        self.conv1 = ConvLayer(3, 32, kernel_size=9, stride=1)
+        self.in1 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
+        self.in2 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
+        self.in3 = torch.nn.InstanceNorm2d(128, affine=True)
+        # Residual layers
+        self.res1 = ResidualBlock(128)
+        self.res2 = ResidualBlock(128)
+        self.res3 = ResidualBlock(128)
+        self.res4 = ResidualBlock(128)
+        self.res5 = ResidualBlock(128)
+        # Upsampling Layers
+        self.deconv1 = UpsampleConvLayer(128, 64, kernel_size=3, stride=1, upsample=2)
+        self.in4 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.deconv2 = UpsampleConvLayer(64, 32, kernel_size=3, stride=1, upsample=2)
+        self.in5 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.deconv3 = ConvLayer(32, 3, kernel_size=9, stride=1)
+        # Non-linearities
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, X):
+        y = self.relu(self.in1(self.conv1(X)))
+        y = self.relu(self.in2(self.conv2(y)))
+        y = self.relu(self.in3(self.conv3(y)))
+        y = self.res1(y)
+        y = self.res2(y)
+        y = self.res3(y)
+        y = self.res4(y)
+        y = self.res5(y)
+        y = self.relu(self.in4(self.deconv1(y)))
+        y = self.relu(self.in5(self.deconv2(y)))
+        y = self.deconv3(y)
+        return y
+
+
+class ConvLayer(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super(ConvLayer, self).__init__()
+        reflection_padding = kernel_size // 2
+        self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
+        self.conv2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x):
+        out = self.reflection_pad(x)
+        out = self.conv2d(out)
+        return out
+
+
+class ResidualBlock(torch.nn.Module):
+    """ResidualBlock
+    introduced in: https://arxiv.org/abs/1512.03385
+    recommended architecture: http://torch.ch/blog/2016/02/04/resnets.html
+    """
+
+    def __init__(self, channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = ConvLayer(channels, channels, kernel_size=3, stride=1)
+        self.in1 = torch.nn.InstanceNorm2d(channels, affine=True)
+        self.conv2 = ConvLayer(channels, channels, kernel_size=3, stride=1)
+        self.in2 = torch.nn.InstanceNorm2d(channels, affine=True)
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, x):
+        residual = x
+        out = self.relu(self.in1(self.conv1(x)))
+        out = self.in2(self.conv2(out))
+        out = out + residual
+        return out
+
+
+class UpsampleConvLayer(torch.nn.Module):
+    """UpsampleConvLayer
+    Upsamples the input and then does a convolution. This method gives better results
+    compared to ConvTranspose2d.
+    ref: http://distill.pub/2016/deconv-checkerboard/
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride, upsample=None):
+        super(UpsampleConvLayer, self).__init__()
+        self.upsample = upsample
+        reflection_padding = kernel_size // 2
+        self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
+        self.conv2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x):
+        x_in = x
+        if self.upsample:
+            x_in = torch.nn.functional.interpolate(x_in, mode='nearest', scale_factor=self.upsample)
+        out = self.reflection_pad(x_in)
+        out = self.conv2d(out)
+        return out
+
+
+class FeatureLoss(torch.nn.Module): 
+    def __init__(self, style_img, m_feat, layer_ids, layer_wgts):
         super().__init__()
+        self.style_img = style_img
+        self.m_feat = m_feat
+        self.loss_features = [self.m_feat[i] for i in layer_ids]
+        self.hooks = hook_outputs(self.loss_features, detach=False)
+        self.wgts = layer_wgts
+        self.metric_names = ['tv(reg)'] + ['content'] + [f'gram_{i}' for i in range(len(layer_ids))]
+        
+        
+    def gram_matrix(self,x):
+        n,c,h,w = x.size()
+        x = x.view(n, c, -1)
+        return (x @ x.transpose(1,2))/(c*h*w)
+
     def make_features(self, x, clone=False):
-        return []
-    def forward(self, input, target):
-        return target.mean()
+        self.m_feat(x)
+        return [(o.clone() if clone else o) for o in self.hooks.stored]
+    
+    def forward(self, gen_img, cont_img):
+        cont_feat = self.make_features(cont_img)
+        style_feat = self.make_features(self.style_img, clone=True)
+        gen_feat = self.make_features(gen_img)
+        
+        self.feat_losses = [ ( ((gen_img[:,:,1:,:] - gen_img[:,:,:-1,:]).pow(2)).mean() +
+                                ((gen_img[:,:,:,1:] - gen_img[:,:,:,:-1]).pow(2)).mean() ) * 1e-1 ]
+        
+        self.feat_losses += [ F.mse_loss(gen_feat[1], cont_feat[1]) * 1e5 ]  #content loss (activations)
+
+        self.feat_losses += [ F.mse_loss(self.gram_matrix(f_in), self.gram_matrix(f_out)) * w
+                             for f_in, f_out, w in zip(gen_feat, style_feat, self.wgts) ] #style loss
+
+        self.metrics = dict(zip(self.metric_names, self.feat_losses))
+
+        return sum(self.feat_losses)
+    
     def __del__(self): self.hooks.remove()
 
-def round_up_to_even(f):
-    return math.ceil(f / 2.) * 2
 
-def get_resize(y, z, max_size):
-    if y*2 <= max_size and z*2 <= max_size:
-        y_new = y*2
-        z_new = z*2
-    else:
-        if y > z:
-            y_new = max_size
-            z_new = int(round_up_to_even(z * max_size / y))
-
-        else:
-            z_new = max_size
-            y_new = int(round_up_to_even(y * max_size / z))
-    return (y_new, z_new)
+    
