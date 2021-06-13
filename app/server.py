19e868d2d82cf3c37b3dd65e22eea7790c122f83
amancrackpot <59706418+amancrackpot@users.noreply.github.com>
2021-06-13 23:35:04 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -5,15 +5,11 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.templating import Jinja2Templates
 import uvicorn, aiohttp, asyncio
 from io import BytesIO, StringIO
-from fastai import *
-from fastai.vision import *
+from fastai.vision.all import *
 import base64
 import pdb
-from utils import *
 
-export_file_url = 'https://www.googleapis.com/drive/v3/files/1z5WRMshbw8Xz38UPU2-Fulk0f1ncU90B?alt=media&key=AIzaSyDvMHW-2yleU8G3OljOhzT49zTtf91xuYU'
 export_file_name = 'export.pkl'
-classes = ['a', 'b', 'c']
 
 path = Path(__file__).parent
 
@@ -33,7 +29,7 @@ async def download_file(url, dest):
 async def setup_learner():
 #     await download_file(export_file_url, path/'models'/export_file_name)
     defaults.device = torch.device('cpu')
-    learn = load_learner(path/'saved', export_file_name)
+    learn = load_learner(path/export_file_name)
     return learn
 
 loop = asyncio.get_event_loop()
@@ -45,38 +41,19 @@ loop.close()
 async def upload(request):
     data = await request.form()
     img_bytes = await (data["file"].read())
-
-    img = open_image(BytesIO(img_bytes))
-    w, h = img.size   
+    img = PILImage.create(BytesIO(img_bytes))
+    orig_size = img.size   
     fi = float(data["fi"])
     Style = data["Style"]
-    size_ = int(256*(w/h)*fi), int(256*fi)
-
-    data_ = ( ImageImageList.from_folder(path=path, ignore_empty=True, recurse=False)
-                    .split_none()
-                    .label_empty()
-                    .transform(size=size_)
-                    .databunch(bs=1)
-                    .normalize(imagenet_stats, do_y=True) )
-    data_.c = 3
-
-    learn.data = data_
-    learn.model_dir = '.'
-    learn.load(Style)
     
-    _,img_hr,losses = learn.predict(img)
-
-    im = Image(img_hr.clamp(0,1))
-
-    im_data = image2np(im.data*255).astype(np.uint8)
-
-    img_io = BytesIO()
-
-    PIL.Image.fromarray(im_data).save(img_io, 'PNG')
-
-    img_io.seek(0)
-
-    img_str = base64.b64encode(img_io.getvalue()).decode()
+    learn.dls.valid.after_item  = Pipeline([RatioResize(int(256*fi)), ToTensor()])
+    learn.model_dir = '.'
+    learner.load(Style, device='cpu', with_opt=False)
+    
+    pred_img = learner.predict(img)[0]
+    pred_img = PILImage.create(pred_img).resize(orig_size)
+    pred_img_bytes = pred_img.to_bytes_format()
+    img_str = base64.b64encode(pred_img_bytes).decode()
     img_str = "data:image/png;base64," + img_str
 
     return templates.TemplateResponse('output.html', {'request' : request, 'b64val' : img_str})
